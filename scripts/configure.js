76cbbfb75cdcc3c07ebacec34069dbcd307f7a23
Howard Hellyer <hhellyer@uk.ibm.com>
2016-12-12 14:15:23 +0000

    Improve detection of lldb executable on Linux. - If lldb doesn't exist try lldb-3.9,3.8,3.7 etc instead. - Allow install scripts guess to be overridden with --lldb_exe option   e.g. npm install --lldb_exe=`which lldb-3.9`

--- scripts/configure.js
+++ scripts/configure.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const child_process = require('child_process');
 
 const lldbReleases = {
+  '4.0': 'master',
   '3.9': 'release_39',
   '3.8': 'release_38',
   '3.7': 'release_37',
@@ -24,6 +25,8 @@ var lldbIncludeDir;
 // Need to determine:
 // - What level of lldb we are running.
 // - If we need the headers. (Linux may have them installed)
+var lldbExe = 'lldb';
+
 if (osName === 'Darwin') {
 
   lldbVersion = getDarwinRelease();
@@ -38,7 +41,8 @@ if (osName === 'Darwin') {
 
 } else if ( osName === 'Linux') {
 
-  lldbVersion = getLinuxVersion();
+  lldbExe = getLldbExecutable();
+  lldbVersion = getLinuxVersion(lldbExe);
 
   if(lldbVersion === undefined) {
     console.log('Unable to locate lldb binary. llnode installation failed.');
@@ -57,7 +61,7 @@ if (osName === 'Darwin') {
   }
 }
 
-console.log('Installing llnode for lldb version ' + lldbVersion);
+console.log('Installing llnode for ' + lldbExe + ', lldb version ' + lldbVersion);
 
 // Check out source code of the LLDB that is compatible with OS X's default lldb
 // TODO: The llvm project is probably moving to github soon at that point we
@@ -113,21 +117,43 @@ function getDarwinRelease() {
   }
 }
 
-// There are multiple versions of lldb available for the various linux distos.
-// Use the default unless --llnode_version= has been set on the command line.
-function getLinuxVersion() {
+// Find the 'best' lldb to use. Either:
+// - the one specified by the user using npm --llnode_exe=... install llnode
+// - the default lldb executable
+// - the higest known lldb version
+function getLldbExecutable() {
+  var lldbExe = process.env.npm_config_lldb_exe;
+  if (lldbExe === undefined) {
+    var lldbExeNames = ['lldb', 'lldb-4.0', 'lldb-3.9', 'lldb-3.8', 'lldb-3.7', 'lldb-3.6']
+    for (lldbExeVersion of lldbExeNames) {
+      try {
+        lldbExe = child_process.execSync('which ' + lldbExeVersion).toString().trim();
+        // If the result starts with '/' `which` found a path.
+        if (lldbExe.startsWith('/')) {
+          break;
+        }
+      } catch(err) {
+        // Do nothing - we expect not to find some of these.
+      }
+    }
+  }
+  return lldbExe;
+}
 
-  // TODO - Allow the user to specify the version of lldb to use, e.g:
-  // lldb-3.9 instead of lldb as Linux has packages for multiple versions
-  // of lldb.
+// There are multiple versions of lldb available for the various linux distos.
+// Use the default unless --llnode_exe= has been set on the command line.
+function getLinuxVersion(lldbExe) {
+
   var lldbStr;
   try {
-    lldbStr = child_process.execFileSync('lldb', ['-v']).toString();
+    lldbStr = child_process.execFileSync(lldbExe, ['-v']).toString();
   } catch(err) {
     return undefined;
   }
   // Ignore minor revisions like 3.8.1
-  if (lldbStr.indexOf('version 3.9') > 0) {
+  if (lldbStr.indexOf('version 4.0') > 0) {
+    return '4.0';
+  } else if (lldbStr.indexOf('version 3.9') > 0) {
     return '3.9';
   } else if (lldbStr.indexOf('version 3.8') > 0) {
     return '3.8';
