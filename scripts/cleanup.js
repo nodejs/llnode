efab344b1b90e8ce2ade46d1a956c8613a61f88c
Richard Chamberlain <richard_chamberlain@uk.ibm.com>
2017-03-03 13:14:33 +0000

    readme: fix and docs for lldb system plugin dir
    Fix makefile to install plugin as system plugin for linux in
    /usr/lib/lldb/plugins. Add README docs for use of lldb system
    plugin directory
    
    PR-URL: https://github.com/nodejs/llnode/pull/72
    Reviewed-By: Howard Hellyer <hhellyer@uk.ibm.com>
    Reviewed-By: Fedor Indutny <fedor@indutny.com>

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -18,4 +18,5 @@ console.log(`Copying ${cwd}/out/Release/${llnodeLib} to ${cwd}/${llnodeLib}`);
 child_process.execSync(`mv ${cwd}/out/Release/${llnodeLib} ${cwd}/${llnodeLib}`);
 
 console.log(`${os.EOL}llnode plugin installed, load in lldb with:`);
-console.log(`(lldb) plugin load ${cwd}/${llnodeLib}${os.EOL}`);
+console.log(`(lldb) plugin load ${cwd}/${llnodeLib}`);
+console.log(`or copy plugin to lldb system plugin directory, see www.npmjs.org/llnode${os.EOL}`);
